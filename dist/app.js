da6cbb3b21036629a5e7f4eeeac2e5de8e557212
Mohammed Aman Mev <mohammedaman0731@gmail.com>
2024-01-30 18:37:50 +0530

    progress

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 const port = 8000;
+app.get;
 app.listen(port, () => {
     console.log(`server express is working on port:${port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+app.get
 
 app.listen(port, () => {
   console.log(`server express is working on port:${port}`);
